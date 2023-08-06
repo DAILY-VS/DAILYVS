72ddbef9b5de2fcfc193772a9df0189e28dd7bad
minkyung_kwak <ellen10min@gmail.com>
2023-08-06 17:42:49 +0900

    [Add] 회원가입 디자인

--- server/account/forms.py
+++ server/account/forms.py
@@ -4,21 +4,66 @@ from .models import User
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserChangeForm
 
+
 class SignupForm(UserCreationForm):
-    username = forms.CharField(label='Username', max_length=100, min_length=5,
-                               widget=forms.TextInput(attrs={'class': 'form-control'}))
-    password1 = forms.CharField(label='Password', max_length=50, min_length=5,
-                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
-    password2 = forms.CharField(label='Confirm Password',
-                                max_length=50, min_length=5,
-                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(
+        label="아이디",
+        max_length=100,
+        min_length=5,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "아이디를 입력하세요"}
+        ),
+    )
+    password1 = forms.CharField(
+        label="비밀번호",
+        max_length=50,
+        min_length=5,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "비밀번호"}
+        ),
+    )
+    password2 = forms.CharField(
+        label="확인 비밀번호",
+        max_length=50,
+        min_length=5,
+        widget=forms.PasswordInput(
+            attrs={"class": "form-control", "placeholder": "비밀번호 확인"}
+        ),
+    )
+    gender_choices = [
+        ("male", "남자"),
+        ("female", "여자"),
+    ]
+    gender = forms.ChoiceField(
+        label="성별",
+        choices=gender_choices,
+        widget=forms.RadioSelect(attrs={"class": "radio-list"}),
+    )
 
     class Meta:
         model = User
-        fields = ['username', 'password1', 'password2','mbti','gender','nickname']
-        
+        fields = ["username", "password1", "password2", "mbti", "nickname", "gender"]
+        widgets = {
+            "mbti": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "MBTI를 입력하세요"}
+            ),
+            "nickname": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "별명을 입력하세요"}
+            ),
+        }
+
+
 class UserChangeForm(UserChangeForm):
     password = None
+
     class Meta:
         model = get_user_model()
-        fields = ['nickname','mbti','gender']
+        fields = ["nickname", "mbti", "gender"]
+        widgets = {
+            "mbti": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "MBTI를 입력하세요"}
+            ),
+            "gender": forms.TextInput(
+                attrs={"class": "form-control", "placeholder": "성별을 입력하세요"}
+            ),
+        }
