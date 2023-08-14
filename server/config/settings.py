3ae418fc5cef603366312b2166e4a7f52be7f232
hasoleee <wsw3727@gmail.com>
2023-08-15 05:34:16 +0900

    [add] 배포 전 세팅 변경

--- server/config/settings.py
+++ server/config/settings.py
@@ -118,10 +118,10 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
-if DEBUG == True:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-else:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# if DEBUG == True:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# else:
+#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- server/config/wsgi.py
+++ server/config/wsgi.py
@@ -1,7 +1,7 @@
 import os
-import site
+# import site
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+# site.addsitedir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
 application = get_wsgi_application()
