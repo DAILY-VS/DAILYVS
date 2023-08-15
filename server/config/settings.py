1c29162be079584815090f2465cbde8f650f9ed2
hasoleee <wsw3727@gmail.com>
2023-08-15 21:57:04 +0900

    마이그레이션

--- server/config/settings.py
+++ server/config/settings.py
@@ -68,13 +68,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-
-DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'HOST': 'daily-vs-db.cluster-cjlrrga1cato.ap-northeast-2.rds.amazonaws.com',
