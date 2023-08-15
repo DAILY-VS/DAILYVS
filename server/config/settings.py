d653d59bf959089c234ccb648f9ffc57e167a7c1
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-08-15 17:34:08 +0900

    dd

--- server/config/settings.py
+++ server/config/settings.py
@@ -130,7 +130,6 @@ SITE_ID = 1
 
 AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',    
-    'allauth.account.auth_backends.AuthenticationBackend',
 ]
 
 # Provider specific settings
