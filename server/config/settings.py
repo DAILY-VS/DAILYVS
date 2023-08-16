abf1846c18d67d93890d978426a3903df4fe8a6a
hasoleee <wsw3727@gmail.com>
2023-08-16 13:24:13 +0900

    [add] settings.py media 변경

--- server/config/settings.py
+++ server/config/settings.py
@@ -5,8 +5,7 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 ROOT_DIR = os.path.dirname(BASE_DIR)
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -116,6 +115,10 @@ AUTH_USER_MODEL = "account.User"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATIC_URL = "/static/"
 
