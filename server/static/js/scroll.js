c679998347de33364537a3931a47dee3bb6897f8
minkyung_kwak <ellen10min@gmail.com>
2023-08-18 17:12:42 +0900

    [Add] scroll.js 오류 수정

--- server/static/js/scroll.js
+++ server/static/js/scroll.js
@@ -1,26 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const resultContainer = document.getElementById("capture");
-  const blueBgColor = "#f9fcff";
-  const purpleBgColor = "#f8f8ff";
-  const pinkBgColor = "#fff9f9";
-  let currentBgColor = pinkBgColor;
+  const resultContainer = document.querySelector(".result-container");
+  if (resultContainer) {
+    const blueBgColor = "#f9fcff";
+    const purpleBgColor = "#f8f8ff";
+    const pinkBgColor = "#fff9f9";
+    let currentBgColor = pinkBgColor;
 
-  window.addEventListener("scroll", function () {
-    const scrollY = window.scrollY;
+    window.addEventListener("scroll", function () {
+      const scrollY = window.scrollY;
 
-    // 스크롤 위치에 따라 배경색 변경
-    if (scrollY >= 0 && scrollY < 500) {
-      resultContainer.style.backgroundColor = pinkBgColor;
-      console.log(currentBgColor);
-    } else if (scrollY >= 500 && scrollY < 1000) {
-      resultContainer.style.backgroundColor = purpleBgColor;
-    } else {
-      resultContainer.style.backgroundColor = blueBgColor;
-    }
-  });
+      // 스크롤 위치에 따라 배경색 변경
+      if (scrollY >= 0 && scrollY < 500) {
+        resultContainer.style.backgroundColor = pinkBgColor;
+        console.log(currentBgColor);
+      } else if (scrollY >= 500 && scrollY < 1000) {
+        resultContainer.style.backgroundColor = purpleBgColor;
+      } else {
+        resultContainer.style.backgroundColor = blueBgColor;
+      }
+    });
 
-  // 초기 배경색 설정
-  resultContainer.style.backgroundColor = currentBgColor;
-
-
+    // 초기 배경색 설정
+ resultContainer.style.backgroundColor = currentBgColor;
+  } else {
+    console.error("Element with ID 'capture' not found.");
+  }
 });
