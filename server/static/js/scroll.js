1c882fc9d602b60f21f620be38e896b333c05282
minkyung_kwak <ellen10min@gmail.com>
2023-08-19 11:42:12 +0900

    [Add] result CSS 수정

--- server/static/js/scroll.js
+++ server/static/js/scroll.js
@@ -12,7 +12,6 @@ document.addEventListener("DOMContentLoaded", function () {
       // 스크롤 위치에 따라 배경색 변경
       if (scrollY >= 0 && scrollY < 500) {
         resultContainer.style.backgroundColor = pinkBgColor;
-        console.log(currentBgColor);
       } else if (scrollY >= 500 && scrollY < 1000) {
         resultContainer.style.backgroundColor = purpleBgColor;
       } else {
